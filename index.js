e7efe26dd84e1e7544829bc337456be4f8057463
ckapucu <ckapucu@gmail.com>
2016-10-04 16:34:10 +0300

    panel data eklendi

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var mysql   = require("mysql");
 var bodyParser  = require("body-parser");
+//var stormpath = require('express-stormpath');
 var md5 = require('md5');
 var rest = require("./rest.js");
 var app  = express();
@@ -15,7 +16,7 @@ REST.prototype.connectMysql = function() {
     var self = this;
     var pool      =    mysql.createPool({
         connectionLimit : 100,
-        host     : 'petunia.arvixe.com',
+        host     : 'localhost',
         user     : 'ckapucu',
         password : 'M2667k9363',
         database : 'ck_deneme',
@@ -41,9 +42,29 @@ REST.prototype.configureExpress = function(connection) {
 }
 
 REST.prototype.startServer = function() {
+
+      //BEN EKLEDIM
+      /*
+      app.use(stormpath.init(app, {
+          //apiKeyFile: '/.stormpath/apiKey.properties',
+          apiKeyId:     process.env.STORMPATH_API_KEY_ID || '2MX20ON9RGIMPKFBAK5FKL1UJ',
+          apiKeySecret: process.env.STORMPATH_API_KEY_SECRET || 'OfxQQrZKz1QjJHYlDj7qhPKAx+JQUTD2mWk4oyNZXdU',
+          application:  process.env.STORMPATH_URL || 'https://api.stormpath.com/v1/applications/1SbZDyiJDSKabH9wtRC8hr'
+      }));
+      /**/
+
+      //app.use(stormpath.init(app, { }));
+
       app.listen(port,function(){
           console.log("All right ! I am alive at Port " + port);
       });
+
+      /*
+      app.on('stormpath.ready', function() {
+          app.listen(port,function(){
+            console.log("All right ! I am alive at Port " + port);
+          });
+      });/**/
 }
 
 REST.prototype.stop = function(err) {
--- rest.js
+++ rest.js
@@ -1,4 +1,9 @@
 //https://codeforgeek.com/2015/03/restful-api-node-and-express-4/
+//diğer bir alternatif https://www.npmjs.com/package/mysql-to-rest
+
+//bu adrestede dosyaya ayzdıran ve oradan okuyanı var https://www.tutorialspoint.com/nodejs/nodejs_restful_api.htm
+
+//security için denenebilir https://stormpath.com/blog/tutorial-build-rest-api-mobile-apps-using-node-js
 var mysql = require("mysql");
 function REST_ROUTER(router,connection,md5) {
     var self = this;
@@ -9,6 +14,7 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
     router.get("/",function(req,res){
            res.json({"Message" : "Hello World !"});
     });
+
     router.post("/users",function(req,res){
         var query = "INSERT INTO ??(??,??) VALUES (?,?)";
         var table = ["users","name","email",req.body.name,req.body.email];
@@ -89,6 +95,60 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
             }
         });
     });
+
+    //PANEL_DATA pandat diye kısaltalım http verb olarak
+    router.post("/pandat",function(req,res){
+        var query = "INSERT INTO ??(??,??,??,??,??,??,??) VALUES (?,?,?,?,?,?,?)";
+        var table = ["panel_data","panel_id","voc","isc","irradiation","cell_temp","amb_temp","humidity", 
+        			req.body.panel_id, req.body.voc, req.body.isc, req.body.irradiation, req.body.cell_temp, req.body.amb_temp, req.body.humidity];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Panel Data Added !"});
+            }
+        });
+    });
+
+    router.get("/pandat",function(req,res){
+        var query = "SELECT * FROM ??";
+        var table = ["panel_data"];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Pandat" : rows});
+            }
+        });
+    });
+
+    router.get("/pandat/:panel_id",function(req,res){
+        var query = "SELECT * FROM ?? WHERE ??=? ORDER BY created";
+        var table = ["panel_data","panel_id",req.params.panel_id];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Pandat" : rows});
+            }
+        });
+    });
+
+    router.delete("/pandat/:panel_id",function(req,res){
+        var query = "DELETE from ?? WHERE ??=?";
+        var table = ["panel_data","panel_id",req.params.panel_id];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Deleted all panel data with panel id "+req.params.panel_id});
+            }
+        });
+    });    
 
 }
 
