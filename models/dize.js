802294e86ef2a172dc47b8be6d29ac75cbdc7d1d
ckapucu <ckapucu@gmail.com>
2018-07-25 16:26:15 +0300

    aynesssssssa

--- models/dize.js
+++ models/dize.js
@@ -4,7 +4,7 @@ function Dize() {
 
   this.get = function(res) {
     connection.acquire(function(err, con) {
-      con.query('select * from module_data', function(err, result) {
+      con.query('select * from string_data', function(err, result) {
         con.release();
         res.send(result);
       });
@@ -13,7 +13,7 @@ function Dize() {
 
   this.get = function(recordno, res) {
     connection.acquire(function(err, con) {
-      con.query('select * from module_data where recordno = ?', [recordno], function(err, result) {
+      con.query('select * from string_data where recordno = ?', [recordno], function(err, result) {
         con.release();
         /*if (err) {
           res.send({status: 1, message: 'Failed to select'});
@@ -24,9 +24,9 @@ function Dize() {
     });
   };
 
-  this.get = function(module_id, res) {
+  this.get = function(string_id, res) {
     connection.acquire(function(err, con) {
-      con.query('select * from module_data where module_id = ?', [module_id], function(err, result) {
+      con.query('select * from string_data where string_id = ?', [string_id], function(err, result) {
         con.release();
         /*if (err) {
           res.send({status: 1, message: 'Failed to select'});
@@ -39,7 +39,7 @@ function Dize() {
 
   this.create = function(dize, res) {
     connection.acquire(function(err, con) {
-      con.query('insert into module_data set ?', dize, function(err, result) {
+      con.query('insert into string_data set ?', dize, function(err, result) {
         con.release();
         if (err) {
           res.send({status: 1, message: 'String data creation failed'});
@@ -52,7 +52,7 @@ function Dize() {
 
   this.update = function(dize, res) {
     connection.acquire(function(err, con) {
-      con.query('update module_data set ? where recordno = ?', [dize, dize.recordno], function(err, result) {
+      con.query('update string_data set ? where recordno = ?', [dize, dize.recordno], function(err, result) {
         con.release();
         if (err) {
           res.send({status: 1, message: 'String data update failed'});
@@ -65,7 +65,7 @@ function Dize() {
 
   this.delete = function(recordno, res) {
     connection.acquire(function(err, con) {
-      con.query('delete from module_data where recordno = ?', [recordno], function(err, result) {
+      con.query('delete from string_data where recordno = ?', [recordno], function(err, result) {
         con.release();
         if (err) {
           res.send({status: 1, message: 'Failed to delete'});
