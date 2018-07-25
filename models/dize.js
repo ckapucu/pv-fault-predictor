aa3a6e6604995c5a87094d8c60e59a15042a0389
ckapucu <ckapucu@gmail.com>
2018-07-25 16:37:28 +0300

    ay

--- models/dize.js
+++ models/dize.js
@@ -9,33 +9,7 @@ function Dize() {
         res.send(result);
       });
     });
-  };
-
-  this.get = function(recordno, res) {
-    connection.acquire(function(err, con) {
-      con.query('select * from string_data where recordno = ?', [recordno], function(err, result) {
-        con.release();
-        /*if (err) {
-          res.send({status: 1, message: 'Failed to select'});
-        } else {
-          res.send({status: 0, message: 'Selected successfully'});*/
-        res.send(result);
-      });
-    });
-  };
-
-  this.get = function(string_id, res) {
-    connection.acquire(function(err, con) {
-      con.query('select * from string_data where string_id = ?', [string_id], function(err, result) {
-        con.release();
-        /*if (err) {
-          res.send({status: 1, message: 'Failed to select'});
-        } else {
-          res.send({status: 0, message: 'Selected successfully'});*/
-        res.send(result);
-      });
-    });
-  };    
+  };   
 
   this.create = function(dize, res) {
     connection.acquire(function(err, con) {
@@ -45,32 +19,6 @@ function Dize() {
           res.send({status: 1, message: 'String data creation failed'});
         } else {
           res.send({status: 0, message: 'String data created successfully'});
-        }
-      });
-    });
-  };
-
-  this.update = function(dize, res) {
-    connection.acquire(function(err, con) {
-      con.query('update string_data set ? where recordno = ?', [dize, dize.recordno], function(err, result) {
-        con.release();
-        if (err) {
-          res.send({status: 1, message: 'String data update failed'});
-        } else {
-          res.send({status: 0, message: 'String data updated successfully'});
-        }
-      });
-    });
-  };
-
-  this.delete = function(recordno, res) {
-    connection.acquire(function(err, con) {
-      con.query('delete from string_data where recordno = ?', [recordno], function(err, result) {
-        con.release();
-        if (err) {
-          res.send({status: 1, message: 'Failed to delete'});
-        } else {
-          res.send({status: 0, message: 'Deleted successfully'});
         }
       });
     });
